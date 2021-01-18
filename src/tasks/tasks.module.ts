2edc254109472d0de6b6778c85283a09185a46d4
unknown <hosein.abolhasani1998@gmail.com>
2021-01-18 15:28:13 +0330

    added useGuards for tasks controller

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,3 +1,4 @@
+import { AuthModule } from './../auth/auth.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepository } from './task.repository';
@@ -5,7 +6,7 @@ import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskRepository])],
+  imports: [TypeOrmModule.forFeature([TaskRepository]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
